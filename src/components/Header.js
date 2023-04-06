663ba7368d6f7b27bbf498bec137d14960b41bba
alexiscenriquez <alexiscenriquez@hotmail.com>
2023-04-06 15:29:10 -0500

    Auth page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1 className="display-2">Products Website</h1>
-            <h2>Hello World</h2>
+            {/* <h1 className="display-2">Products Website</h1>
+            <h2>Hello World</h2> */}
         </div>
     );
 };
