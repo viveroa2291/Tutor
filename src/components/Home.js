5a83aa12cc26d1232b495ffd3a982cb545053b47
viveroa2291 <viveroa2291@uwec.edu>
2023-04-07 09:35:47 -0500

    Nothing to see here
    Nothing to see here.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,10 @@ import Header from "./Header";
 const Home = () => {
     <Header/>
     return (
-        <div>
-            <h2>Welcome to the home page</h2>
-        </div>
+        <section>
+            
+        </section>
+        
     );
 };
 
