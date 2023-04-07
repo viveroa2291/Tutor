b5672fb29a60c0fbb90a4b8894d3cfdf7b6cfe3b
viveroa2291 <viveroa2291@uwec.edu>
2023-04-07 11:55:33 -0500

    Set up the tutors page
    This page will pull down a list of tutors.

---
+++ src/components/Tutor.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "./Header";
+import { Link } from 'react-router-dom';
+
+const Tutor = () => {
+    <Header/>
+    return (
+        <section>
+            <section>
+               <h2>Here is a list of our tutors.</h2>
+            </section>
+            
+        </section>
+        
+    );
+};
+
+export default Tutor;
