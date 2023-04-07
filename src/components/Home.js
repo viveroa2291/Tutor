4763c34f1909a05d2a48a18dc1319f1b0ee71ec5
viveroa2291 <viveroa2291@uwec.edu>
2023-04-07 11:13:37 -0500

    Home page Finished
    Finished the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Header from "./Header";
+import { Link } from 'react-router-dom';
+import './CSS/home.css';
+import tutor from '../images/tutor-home.jpeg';
 const Home = () => {
     <Header/>
     return (
         <section>
+            <section class="home-body">
+               <div class="left-div">
+                    <h2>World Class Tutoring Network</h2>
+                    <hr className='hr-title'/>
+                    <p class="body-text">Here at Mara Tutoring, we link you up with a tutor that you feel is best fit for you with an endless amount of subjects. </p>
+                    <div class="started"><Link class="get-started" to="/signup">Get Started</Link></div>
+                </div> 
+                <div className='right-div'>
+                    <img src={tutor} alt="Tutoring Session" className='tutoring'/>
+                </div>
+            </section>
             
         </section>
         
