575e1c6aa0e27ae8cd27ec4c2603739f21a5053d
viveroa2291 <viveroa2291@uwec.edu>
2023-04-09 18:27:18 -0500

    Hamburger Finished
    Finished the Hamburger when minimized.

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,6 +31,26 @@ const Header = (props) => {
     localStorage.removeItem('username');
   }
 */
+  function myRotate(x) {    
+    x.classList.add('my-class');
+    x.classList.toggle("change");
+  }   
+  function toggleNav() {    
+    let elements = document.getElementsByClassName('top-nav');
+    if(document.getElementById('hamburger').style.marginBottom === "10rem") {
+        document.getElementById('hamburger').style.marginBottom="0";
+    }
+    else {
+       document.getElementById('hamburger').style.marginBottom="10rem"; 
+    }
+    for(let i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle('show');
+    }
+}
+  function handleNav() {
+    myRotate(document.getElementById('hamburger'));
+    toggleNav();
+  }
     return (
         
     <section>
@@ -42,7 +62,7 @@ const Header = (props) => {
             </div>
             <hr className="bottom-line"/>
         </div>
-        <div id="hamburger" className="hamburger">
+        <div id="hamburger" className="hamburger" onClick={handleNav}>
             <hr className='hr1'/>
             <hr className='hr2'/>
             <hr className='hr3'/>
